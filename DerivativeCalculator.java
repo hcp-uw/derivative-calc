aa9e3ff72d8aad28689071ce83c5bda08766c53a
Andrew Kim <ajk47@uw.edu>
2022-04-05 13:52:07 -0700

    updated version

--- DerivativeCalculator.java
+++ DerivativeCalculator.java
@@ -1,7 +1,9 @@
 import java.util.Stack;
+import java.lang.Character;
 
 public class DerivativeCalculator {
     private final char[] operators = {'+', '*', '-', '/', '^'};
+
     public DerivativeCalculator() {
     }
     //  worry about input later
@@ -9,42 +11,45 @@ public class DerivativeCalculator {
     // input Formatting: ex. (x + 1)
     // this sets up the tree
     //
-    public String derive(String input) {
+    public ExpressionNode makeTree(String input) {
         input = input.replaceAll(" ", "");
         Stack<ExpressionNode> stack = new Stack<ExpressionNode>();
         ExpressionNode temp;
         for(int i = 0; i < input.length(); i++) {
-            if(!containsOperator(input.charAt(i))) {
-                temp = new ExpressionNode(input.charAt(i)+ "");
-                stack.push(temp);
-            }
-            else { // if is an operator
-                temp = new ExpressionNode(input.charAt(i) + "");
-                temp.right = stack.pop();
-                temp.left = stack.pop();
-                stack.push(temp);
+            if (!containsOperator(charAt(i)) && !Character.isDigit(input.charAt(i))) {
+                return "Wrong equation";
+            } else {
+                if (!containsOperator(input.charAt(i))) {
+                    temp = new ExpressionNode(input.charAt(i)+ "");
+                    stack.push(temp);
+                }
+                else { // if is an operator
+                    temp = new ExpressionNode(input.charAt(i) + "");
+                    temp.right = stack.pop();
+                    temp.left = stack.pop();
+                    stack.push(temp);
+                }
             }
         }
         temp = stack.pop();
-
-
         temp = derive(temp);
-
-
+        return stack;
     }
 
     // this recurses through the tree
     // should look through the code
+
     private ExpressionNode derive(ExpressionNode root) {
         if (root != null) {
             if (containsOperator(root.value.charAt(0))) {
-                
+                    
             }
         }
     }
 
 
     private String powerRule(String input) {
+        
         return "";
 
     }
--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 	    DerivativeCalculator calc = new DerivativeCalculator();
 
-	    System.out.println(calc.derive("x + 1"));
+	    System.out.println(calc.derive("3 * x + 1"));
     }
 }
